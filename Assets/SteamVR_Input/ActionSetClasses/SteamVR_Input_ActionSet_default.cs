c75166f8eb9e0b1f623306e8d42bcc5074b0ff3f
Elizaveta <105592130+Elizaveta2478@users.noreply.github.com>
2024-11-15 09:54:42 +0300

    Small fix

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -113,6 +113,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean TouchClick
+        {
+            get
+            {
+                return SteamVR_Actions.default_TouchClick;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
